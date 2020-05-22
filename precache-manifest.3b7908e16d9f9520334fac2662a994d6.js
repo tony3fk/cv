de6c2ed00dbcdb1563c3ac61efc4c7bf43d99b75
tony3fk <tony3fk@yahoo.es>
2020-05-22 13:45:19 +0200

    Updates

---
+++ precache-manifest.3b7908e16d9f9520334fac2662a994d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5aa809ee09d34481341f0b8699b2919",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "0746de9f2bed97f7a204",
+    "url": "/cv/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6a70b7a0eda6de83ce25",
+    "url": "/cv/static/js/2.94f62739.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/cv/static/js/2.94f62739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0746de9f2bed97f7a204",
+    "url": "/cv/static/js/main.bd5a29e8.chunk.js"
+  },
+  {
+    "revision": "d5e2b0f5ed4d1535e5ee",
+    "url": "/cv/static/js/runtime-main.f6898fb7.js"
+  }
+]);
